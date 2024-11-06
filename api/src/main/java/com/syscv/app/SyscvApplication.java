67e90bbeb717a9554397d6e85aa9cacd11e30289
Marcelo Raineri <marcelo@gutlz.com>
2024-11-06 19:38:24 +0000

    start project

---
+++ api/src/main/java/com/syscv/app/SyscvApplication.java
@@ -0,0 +1,13 @@
+package com.syscv.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyscvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyscvApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/syscv/app/SyscvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syscv.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyscvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
